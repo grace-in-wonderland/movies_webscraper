f2904a886cbad1f060d98053a873db7877f252be
Grace McPherson <grace.theprogrammer@gmail.com>
2021-03-19 12:16:21 -0400

    scrapping and structuring movie list from imbd

---
+++ main.py
@@ -0,0 +1,61 @@
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+
+headers = {"Accept-Language": "en0US, en;q=0.5"}
+url = "https://www.imdb.com/search/title/?groups=top_1000&ref_adv_prv"
+results = requests.get(url, headers=headers)
+soup = BeautifulSoup(results.text, "html.parser")
+
+titles = []
+years = []
+time = []
+imdb_ratings = []
+metascores = []
+votes = []
+us_gross = []
+
+movie_div = soup.find_all('div', class_='lister-item mode-advanced')
+
+for container in movie_div:
+  name = container.h3.a.text
+  titles.append(name)
+  year = container.h3.find('span', class_='lister-item-year').text
+  years.append(year)
+  runtime = container.find('span', class_='runtime').text if container.p.find('span', class_='runtime') else '-'
+  time.append(runtime)
+  imdb = float(container.strong.text)
+  imdb_ratings.append(imdb)
+  m_score = container.find('span', class_='metascore').text if container.find('span', class_='metascore') else '-'
+  metascores.append(m_score)
+
+  nv = container.find_all('span', attrs={'name': 'nv'})
+  vote = nv[0].text
+  votes.append(vote)
+
+  grosses = nv[1].text if len(nv) > 1 else '-'
+  us_gross.append(grosses)
+  
+movies = pd.DataFrame({
+  'movie': titles,
+  'year': years,
+  'timeMin': time,
+  'imdb': imdb_ratings,
+  'metascores': metascores,
+  'votes': votes,
+  'us_grossMillions': us_gross,
+  })
+
+print(movies)
+
+
+movies['year'] = movies['year'].str.extract('(\d+)').astype(int)
+movies['timeMin'] = movies['timeMin'].str.extract('(\d+)').astype(int)
+movies['metascores'] = pd.to_numeric(movies['metascores'], errors='coerce', downcast='integer')
+movies['votes'] = movies['votes'].str.replace(',', '').astype(int)
+movies['us_grossMillions'] = movies['us_grossMillions'].map(lambda x: x.lstrip('$').rstrip('M'))
+movies['us_grossMillions'] = pd.to_numeric(movies['us_grossMillions'], errors='coerce')
+
+print(movies.dtypes)
+movies.to_csv('movies.csv')
